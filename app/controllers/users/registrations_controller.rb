236490d0c3c60e81dc9b4b8d5419db3aaf280f76
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-02-19 21:07:45 +0100

    create company action and service (only draft)
    [#86386220]

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,20 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def new_company
     @user = User.new
+  end
+
+  def create_company
+    # @user = User.new(create_company_params)
+    # user_creator = UserCompanyCreator.new(create_company_params)
+    # user_creater.save_user
+    # user_creator.create_company
+    # redirect w przypadku sukcesu root
+    # render new_company
+  end
+
+  protected
+
+  def create_company_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :company_name)
+  end
 end
---
+++ app/services/user_company_creator.rb
@@ -0,0 +1,13 @@
+class UserCompanyCreator
+  def initialize(params)
+    @params = params
+  end
+
+  def save_user
+    # create user
+  end
+
+  def create_company
+    # create company and membership
+  end
+end
