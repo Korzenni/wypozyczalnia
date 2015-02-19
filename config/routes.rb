82c3b40b54327d0fed19eb37cac01e2258105814
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-02-19 21:06:38 +0100

    add routes for new company
    [#86386220]

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
+
+  devise_scope :user do
+    get "users/company-sign-up", to: "users/registrations#new_company", as: :new_company
+    post "users/company-sign-up", to: "users/registrations#create_company", as: :create_company
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
