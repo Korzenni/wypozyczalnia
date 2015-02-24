750b637628de264fe955a6563199d53c48f9794f
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-02-24 08:48:45 +0100

    add dashboard route for business partner
    [#86386256]

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-
   devise_for :users
   devise_scope :user do
     get "users/company-sign-up", to: "users/registrations#new_company", as: :new_company
     post "users/company-sign-up", to: "users/registrations#create_company", as: :create_company
+  end
+
+  namespace :business do
+    get "dashboard", to: "dashboard#show", as: :dashboard
   end
 end
