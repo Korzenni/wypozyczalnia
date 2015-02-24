e546af08e0109f31312d6ad79d87828a46be8f5c
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-02-24 18:07:11 +0100

    extract method to application controller (reusable)
    [#86386256]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def redirect_if_not_business_partner
+      redirect_to root_path, notice: 'You are not business partner.' unless current_user.business_partner?
+    end
 end
--- app/controllers/business/dashboard_controller.rb
+++ app/controllers/business/dashboard_controller.rb
@@ -1,16 +1,10 @@
 module Business
-  class DashboardController < ActionController::Base
+  class DashboardController < ApplicationController
     before_action :authenticate_user!
     before_action :redirect_if_not_business_partner
 
     def show
 
-    end
-
-    private
-
-    def redirect_if_not_business_partner
-      redirect_to root_path, notice: 'You are not business partner.' unless current_user.business_partner?
     end
   end
 end
