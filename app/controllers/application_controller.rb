5af49265fbfa372804bf8dd7d83bf9af79e16f2e
adam-szczombrowski <adam.szczombrowski@gmail.com>
2015-03-01 18:00:53 +0100

    Add strong parameters to users   [#86386308]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
 
   def redirect_if_not_business_partner
     redirect_to root_path, notice: 'You are not business partner.' unless current_user.business_partner?
