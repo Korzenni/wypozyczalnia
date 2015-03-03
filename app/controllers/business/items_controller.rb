215afc0639226640a61b3a06a1a0c48ee4561060
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-03-03 08:41:48 +0100

    fix controllers and add initial index view
    [#89172054]

--- app/controllers/business/items_controller.rb
+++ app/controllers/business/items_controller.rb
@@ -1,16 +1,18 @@
-class ItemsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :redirect_if_not_business_partner
+module Business
+  class ItemsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :redirect_if_not_business_partner
 
-  def index
-    @items = current_user.company.items
-  end
+    def index
+      @items = current_user.company.items
+    end
 
-  def show
-    @item = current_user.company.items.find(params[:id])
-  end
+    def show
+      @item = current_user.company.items.find(params[:id])
+    end
 
-  def new
-    @item = Item.new
+    def new
+      @item = Item.new
+    end
   end
 end
