b90714dbed22f7c5c8d490d89ff7ea8d3d5a16bc
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-03-03 08:20:07 +0100

    add validations
    [#89172054]

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :item_category
   belongs_to :company
+
+  validates :company, presence: true
 end
--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -1,4 +1,6 @@
 class ItemCategory < ActiveRecord::Base
   belongs_to :company
   has_many :items
+
+  validates :company, presence: true
 end
