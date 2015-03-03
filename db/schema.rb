6144c69889e7652ae44707de81aecf28db2cff6c
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-03-03 08:05:13 +0100

    add item_category model
    [#89172054]

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224152902) do
+ActiveRecord::Schema.define(version: 20150303070446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "companies", force: true do |t|
     t.string "name"
+  end
+
+  create_table "item_categories", force: true do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.decimal  "deposit"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "memberships", force: true do |t|
