6cdc8f53fbeb84022efec51c7af66f2f4af6bed8
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-03-03 08:19:13 +0100

    add item category to items
    [#89172054]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303071759) do
+ActiveRecord::Schema.define(version: 20150303071851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150303071759) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "company_id"
+    t.integer  "item_category_id"
   end
 
   create_table "memberships", force: true do |t|
