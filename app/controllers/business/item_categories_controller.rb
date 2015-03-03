ea2ebe6961ca6cf75c78db2e480317829eaa87fc
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-03-03 08:30:14 +0100

    add before filters and initial expose to controllers
    [#89172054]

--- app/controllers/business/item_categories_controller.rb
+++ app/controllers/business/item_categories_controller.rb
@@ -1,10 +1,16 @@
 class ItemCategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :redirect_if_not_business_partner
+
   def index
+    @item_categories = current_user.company.item_categories
   end
 
   def show
+    @item_category = current_user.company.item_categories.find(params[:id])
   end
 
   def new
+    @item_category = ItemCategory.new
   end
 end
--- app/controllers/business/items_controller.rb
+++ app/controllers/business/items_controller.rb
@@ -1,10 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :redirect_if_not_business_partner
+
   def index
+    @items = current_user.company.items
   end
 
   def show
+    @item = current_user.company.items.find(params[:id])
   end
 
   def new
+    @item = Item.new
   end
 end
