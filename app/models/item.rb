8efded96ccca07df2d8c521f2ab1a7e2b89b417d
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-03-04 21:30:24 +0100

    add validations to item and item category
    [#89172054]

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,9 @@ class Item < ActiveRecord::Base
   belongs_to :item_category
   belongs_to :company
 
+  validates :name, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :deposit, numericality: { greater_than: 0 }, allow_blank: true
+  validates :inventory_number, presence: true
   validates :company, presence: true
 end
--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -2,5 +2,8 @@ class ItemCategory < ActiveRecord::Base
   belongs_to :company
   has_many :items
 
+  validates :name, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :deposit, numericality: { greater_than: 0 }, allow_blank: true
   validates :company, presence: true
 end
