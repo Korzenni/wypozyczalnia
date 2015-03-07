18ee5bf8889c89c630894169493d7daa0a49cce9
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-03-07 16:09:47 +0100

    add #create for item and item category
    [#89172054]

--- app/controllers/business/item_categories_controller.rb
+++ app/controllers/business/item_categories_controller.rb
@@ -4,7 +4,11 @@ module Business
     before_action :redirect_if_not_business_partner
 
     def index
-      @item_categories = current_user.company.item_categories
+      @item_categories = ItemCategoryQuery.new(params, current_user).call
+      respond_to do |format|
+        format.json { render json: { item_categories: @item_categories } }
+        format.html {}
+      end
     end
 
     def show
@@ -13,6 +17,22 @@ module Business
 
     def new
       @item_category = ItemCategory.new
+    end
+
+    def create
+      @item_category = ItemCategory.new(item_category_params)
+      @item_category.company = current_user.company
+      if @item_category.save
+        redirect_to business_item_categories_path, notice: 'You created item category with success.'
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def item_category_params
+      params.require(:item_category).permit(:name, :price, :deposit, :description)
     end
   end
 end
--- app/controllers/business/items_controller.rb
+++ app/controllers/business/items_controller.rb
@@ -14,5 +14,21 @@ module Business
     def new
       @item = Item.new
     end
+
+    def create
+      @item = Item.new(item_params)
+      @item.company = current_user.company
+      if @item.save
+        redirect_to business_items_path, notice: 'You created item with success.'
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def item_params
+      params.require(:item).permit(:name, :price, :deposit, :description, :inventory_number)
+    end
   end
 end
